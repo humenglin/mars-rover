54f2ececa9a96beafd14beffdbc0215dd8865a3d
humenglin <humenglin@zyb.com.cn>
2020-07-21 21:30:57 +0800

    [refactor]:把MarsRoverPosition类的move、turnLeft、turnRight的方法，提取到MarsRover

--- src/main/java/marsrover/MarsRoverPosition.java
+++ src/main/java/marsrover/MarsRoverPosition.java
@@ -2,9 +2,9 @@ package marsrover;
 
 
 public class MarsRoverPosition {
-    private int coordinatesX;
-    private int coordinatesY;
-    private Direction direction;
+    int coordinatesX;
+    int coordinatesY;
+    Direction direction;
 
     public MarsRoverPosition(int coordinatesX, int coordinatesY, String direction) {
         this.coordinatesX = coordinatesX;
@@ -22,27 +22,6 @@ public class MarsRoverPosition {
 
     public String getDirectionShortName() {
         return direction.getShortName();
-    }
-
-    public void move() {
-        String directionShortName = direction.getShortName();
-        if (directionShortName.equals(Direction.NORTH.getShortName())) {
-            coordinatesY++;
-        } else if (directionShortName.equals(Direction.EAST.getShortName())) {
-            coordinatesX++;
-        } else if (directionShortName.equals(Direction.WEST.getShortName())) {
-            coordinatesX--;
-        } else if (directionShortName.equals(Direction.SOUTH.getShortName())) {
-            coordinatesY--;
-        }
-    }
-
-    public void turnLeft() {
-        direction = direction.leftDirection();
-    }
-
-    public void turnRight() {
-        direction = direction.rightDirection();
     }
 
 }
