09ba2dc18e2136a1ff7f27ba7a4ff4a12ac0adc8
humenglin <humenglin@zyb.com.cn>
2020-07-21 22:02:42 +0800

    [test]：通过测试覆盖率，添加未被覆盖的代码场景的测试

--- src/test/java/marsrover/MarsRoverTest.java
+++ src/test/java/marsrover/MarsRoverTest.java
@@ -40,6 +40,17 @@ public class MarsRoverTest {
     }
 
     @Test
+    public void should_return_x_1_y_1_and_W_when_receive_LLM_command_give_mars_rover_with_init_place_x_2_y_1_E() {
+        MarsRover marsRover = new MarsRover(new MarsRoverPosition(2,1, "E"));
+
+        MarsRoverPosition marsRoverPosition = marsRover.receive("LLM");
+
+        assertThat(marsRoverPosition.getCoordinatesX(), is(1));
+        assertThat(marsRoverPosition.getCoordinatesY(), is(1) );
+        assertThat(marsRoverPosition.getDirectionShortName(), is("W"));
+    }
+
+    @Test
     public void should_return_x_0_y_1_and_N_when_receive_M_command_give_mars_rover_with_init_place_x_0_y_0_N() {
         MarsRover marsRover = new MarsRover(new MarsRoverPosition(0 ,0 , "N"));
 
